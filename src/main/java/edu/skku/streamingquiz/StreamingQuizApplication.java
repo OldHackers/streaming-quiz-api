badc86d2879af0207db4751f0257fe5fa26d93e6
Seungil Kim <ksi990302@gmail.com>
2023-09-22 22:33:07 +0900

    init

---
+++ src/main/java/edu/skku/streamingquiz/StreamingQuizApplication.java
@@ -0,0 +1,13 @@
+package edu.skku.streamingquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/skku/streamingquiz/StreamingQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.skku.streamingquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
